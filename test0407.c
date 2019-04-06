fb6bea3228b61bf84ea4fa5d7a7bbef6a4f98385
aabbcc <iiooiio@hotmail.com>
2019-04-07 00:38:28 +0800

    測試常量與宏定義

---
+++ test0407.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#define URL "http://abc.com.tw"
+#define URL "http://abc.com.uk"
+#define NAME "我是中文"
+int main(){
+
+    printf("%s\n",URL);
+    printf("%s\n",NAME);
+    return 0;
+}
+
